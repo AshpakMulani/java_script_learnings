8640381e0049db346bbef64c664c944969491383
Ashpak Mulani <ashpaklmulani@gmail.com>
2022-07-20 19:35:43 +0530

    basic HTML and JavaScript to get weather details-no css

---
+++ WeatherApp/app.js
@@ -0,0 +1,78 @@
+
+let getWeather = async(location) => {
+const API_KEY = 'eb71cfa3bd3641699dd194037221907';
+let LOCATION = location;
+let API_URL = "http://api.weatherapi.com/v1/current.json?key=" + API_KEY + "&q=" + LOCATION +"&aqi=yes";
+
+return await fetch(API_URL);
+
+};
+
+
+let displayWeather = (data) => {   
+/*
+    if ('error' in data){
+        document.querySelector(".weather-details").style.visibility = "hidden";
+        document.querySelector(".weather-error").style.visibility = "visible";
+
+    }
+    else
+    {
+
+        document.querySelector(".weather-details").style.visibility = "visible";
+        document.querySelector(".weather-error").style.visibility = "hidden";
+*/
+        try{
+        const {name, country, localtime } = data.location;
+        const {temp_c, is_day, condition,
+             wind_kph, humidity, uv } = data.current;
+        
+        document.querySelector(".location-name").innerText = name;
+        document.querySelector(".location-time").innerText = localtime;
+        document.querySelector(".location-country").innerText = country;
+        document.querySelector(".condition-text").innerText = condition.text;
+        document.querySelector(".condition-icon").src = "https:" + condition.icon;
+        document.querySelector(".others-wind_kph").innerText = wind_kph;
+        document.querySelector(".others-humidity").innerText = humidity;
+        document.querySelector(".others-uv").innerText = uv;     
+        document.querySelector(".weather-details").style.visibility = "visible"; 
+        
+        
+        }
+        catch(e){
+            document.querySelector(".weather-details-error").style.visibility = "visible"
+            document.querySelector(".weather-details").style.visibility = "hidden"
+        }
+
+        document.querySelector(".weather-details").classList.remove("loading");
+};
+
+
+let search = () => {
+    let location = document.querySelector(".search-bar input").value
+
+    if (location.length !== 0){
+        document.querySelector(".weather-details").classList.add("loading"); 
+        document.querySelector(".weather-details").style.visibility = "hidden"
+        document.querySelector(".weather-details-error").style.visibility = "hidden";
+
+        getWeather(location)
+        .then((response) => response.json())
+        .then((data) => {displayWeather(data)});
+    }
+}
+
+
+document.querySelector(".search-bar button").addEventListener("click",() => { search() });
+document.querySelector(".search-bar input").addEventListener('keypress', 
+                    function (e) { if (e.key === 'Enter') {  search()  } });
+
+
+
+
+(async function(){
+ await getWeather('London')
+.then((response) => response.json())
+.then((data) => {displayWeather(data)});
+})();
+
